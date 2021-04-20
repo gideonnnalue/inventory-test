7a2a80f87ffdf4d98574541161df16b0f21f8030
Gideon Nnalue <emekannalue@gmail.com>
2021-04-20 23:32:57 +0100

    changed config name

--- src/database/models/index.ts
+++ src/database/models/index.ts
@@ -7,7 +7,7 @@ import Sequelize from 'sequelize';
 
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-const config = require(path.join(__dirname, '/../config/config.js'))[env];
+const config = require(path.join(__dirname, '/../config/config'))[env];
 
 const db: any = {};
 
