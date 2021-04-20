2db862880148c2f44f36f110f7d27014a042d32b
Gideon Nnalue <emekannalue@gmail.com>
2021-04-20 23:50:58 +0100

    logging files

--- src/database/models/index.ts
+++ src/database/models/index.ts
@@ -9,6 +9,8 @@ const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
 const config = require(path.join(__dirname, '/../config/config'))[env];
 
+console.log(config);
+
 const db: any = {};
 
 let sequelize: any;
