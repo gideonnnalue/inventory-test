486a16969060d9b55a8dc9bf3f7a4ce6f2f9104c
Gideon Nnalue <emekannalue@gmail.com>
2021-04-21 01:09:32 +0100

    changed postinstall

--- src/database/migrations/20210415004334-create-item.js
+++ src/database/migrations/20210415004334-create-item.js
@@ -1,9 +1,9 @@
 'use strict';
 module.exports = {
-	down: async (queryInterface: any, Sequelize: any) => {
+	down: async (queryInterface, Sequelize) => {
 		await queryInterface.dropTable('Items');
 	},
-	up: async (queryInterface: any, Sequelize: any) => {
+	up: async (queryInterface, Sequelize) => {
 		await queryInterface.createTable('Items', {
 			createdAt: {
 				allowNull: false,
